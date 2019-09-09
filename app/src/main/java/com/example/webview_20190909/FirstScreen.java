b0731ac80917bd3f92e249a94c1174119ed819e1
motoki_kimura <m_kimura@ksyssol.co.jp>
2019-09-09 16:41:50 +0900

    onClickが反応するように

--- app/src/main/java/com/example/webview_20190909/FirstScreen.java
+++ app/src/main/java/com/example/webview_20190909/FirstScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -21,12 +22,13 @@ public class FirstScreen extends AppCompatActivity {
 
     // 画面遷移用のボタンイベント
     public void onClick(View v){
-        // インテントの作成
-        Intent intent = new Intent(this, FirstScreen.class);
-        EditText editText = this.findViewById(R.id.button);
-        intent.putExtra("sendText",editText.getText().toString());
-//        //遷移先の画面を起動
-        startActivity(intent);
+        Log.v("test","----------------");
+//        // インテントの作成
+//        Intent intent = new Intent(this, FirstScreen.class);
+//        EditText editText = this.findViewById(R.id.button);
+//        intent.putExtra("sendText",editText.getText().toString());
+////        //遷移先の画面を起動
+//        startActivity(intent);
     }
 
 }
