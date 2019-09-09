f417a31846be15e8647b9a011dfc8f234957f1fa
chitoseS <chitosws@gmail.com>
2019-09-09 22:37:03 +0900

    webViewから画面遷移するように変更

--- app/src/main/java/com/example/webview_20190909/WebView_Google.java
+++ app/src/main/java/com/example/webview_20190909/WebView_Google.java
@@ -2,8 +2,11 @@ package com.example.webview_20190909;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -23,5 +26,16 @@ public class WebView_Google extends AppCompatActivity {
         WebView myWebView = findViewById(R.id.webView); // レイアウトに設置したWebViewを追加します。
         myWebView.setWebViewClient(new WebViewClient()); // リンクをクリックしたときに標準ブラウザへ遷移させずにWebView内で表示
         myWebView.loadUrl("https://www.google.com/"); // 表示させたいURLを記述します。
+    }
+
+    // 画面遷移用のボタンイベント
+    public void onClick(View v){
+        Log.v("test","----------------");
+//        // インテントの作成
+        Intent intent = new Intent(this, FirstScreen.class);
+//        EditText editText = this.findViewById(R.id.button);
+//        intent.putExtra("sendText",editText.getText().toString());
+//        //遷移先の画面を起動
+        startActivity(intent);
     }
 }
