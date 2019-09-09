926d1e67d10774829858a7cbe05bc4c07ced8749
motoki_kimura <m_kimura@ksyssol.co.jp>
2019-09-09 17:33:44 +0900

    画面遷移先から元の画面に戻る
    returnボタンの実装

--- app/src/main/java/com/example/webview_20190909/SecondScreen.java
+++ app/src/main/java/com/example/webview_20190909/SecondScreen.java
@@ -2,7 +2,11 @@ package com.example.webview_20190909;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class SecondScreen extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class SecondScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+
+    // 画面遷移用のボタンイベント
+    public void onClick(View v){
+        Log.v("test","----------------");
+//        // インテントの作成
+        Intent intent = new Intent(this, FirstScreen.class);
+//        EditText editText = this.findViewById(R.id.button);
+//        intent.putExtra("sendText",editText.getText().toString());
+//        //遷移先の画面を起動
+        startActivity(intent);
     }
 }
